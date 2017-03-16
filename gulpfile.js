df9253104dc3614d3bf7ef4593bfa6d347aeecc7
David Shusterman <davidshusti@gmail.com>
2017-03-16 20:47:20 +0200

    bundled css files and packages

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 "use strict";
 var gulp = require("gulp");
-var connect = require("gulp-connect"); // Runs a local dev server
-var open = require("gulp-open"); // Open a URL in a web browser
+var gulp_connect = require("gulp-connect"); // Runs a local dev server
+var gulp_open = require("gulp-open"); // gulp_open a URL in a web browser
+var gulp_concat= require('gulp-concat');
 var browserify = require('browserify'); // Bundles JS
 var reactify = require('reactify'); // Transforms React JSX to JS
 var source = require('vinyl-source-stream'); // User conventional text streams with Gulp
+
 
 
 var config = {
@@ -13,14 +15,18 @@ var config = {
     paths: {
         html: './src/*.html',
         js: './src/**/*.js',
+        css: [
+            'node_modules/bootstrap/dist/css/bootstrap.min.css',
+            'node_modules/bootstrap/dist/css/bootstrap-theme.min.css'
+        ],
         dist: './dist',
         mainJs: './src/main.js'
     }
 }
 
 //Start a local development server
-gulp.task('connect', function () {
-    connect.server({
+gulp.task('gulp_connect', function () {
+    gulp_connect.server({
         root: ['dist'],
         port: config.port,
         base: config.devBaseUrl,
@@ -28,9 +34,9 @@ gulp.task('connect', function () {
     });
 });
 
-gulp.task('open', ['connect'], function () {
+gulp.task('gulp_open', ['gulp_connect'], function () {
     gulp.src('dist/index.html')
-        .pipe(open({
+        .pipe(gulp_open({
             uri: config.devBaseUrl + ':' + config.port + '/'
         }));
 });
@@ -38,7 +44,7 @@ gulp.task('open', ['connect'], function () {
 gulp.task('html', function () {
     gulp.src(config.paths.html)
         .pipe(gulp.dest(config.paths.dist))
-        .pipe(connect.reload());
+        .pipe(gulp_connect.reload());
 });
 
 gulp.task('js', function () {
@@ -48,12 +54,18 @@ gulp.task('js', function () {
         .on('error',console.error.bind(console))
         .pipe(source('bundle.js'))
         .pipe(gulp.dest(config.paths.dist + '/scripts'))
-        .pipe(connect.reload());
-})
+        .pipe(gulp_connect.reload());
+});
+
+gulp.task('css', function () {
+   gulp.src(config.paths.css) 
+        .pipe(gulp_concat('bundle.css'))
+        .pipe(gulp.dest(config.paths.dist+'/css'));
+});
 
 gulp.task('watch', function () {
     gulp.watch(config.paths.html, ['html']);
     gulp.watch(config.paths.js, ['js']);
 });
 
-gulp.task('default', ['html','js' ,'open', 'watch']);
+gulp.task('default', ['html','js','css','gulp_open', 'watch']);
